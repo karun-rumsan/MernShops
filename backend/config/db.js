296d42372d9de2a480cee8549351f624cb26d425
Karun <gyawalikarun6@gmail.com>
2021-03-01 01:59:04 +0545

    first

---
+++ backend/config/db.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+  } catch (error) {}
+};
